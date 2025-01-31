517cfcac627958f1959b82b000c4d18a6a11df60
Piotr Kulpinski <piotr@kulpinski.pl>
2025-01-31 14:05:50 +0100

    add basic waitlist desing on the marketing website

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    responsiveImages: true,
+    svg: true,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
