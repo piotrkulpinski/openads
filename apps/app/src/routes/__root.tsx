416a7846f60fa458a080aed762b19cab4626afb0
Piotr Kulpinski <piotr@kulpinski.pl>
2026-04-10 14:53:33 +0200

    chore: remove TanStack devtools dependencies

--- apps/app/src/routes/__root.tsx
+++ apps/app/src/routes/__root.tsx
@@ -1,11 +1,7 @@
 import { SupportProvider } from "@cossistant/react"
 import { Provider as Analytics } from "@openads/events/client"
 import { TooltipProvider } from "@openads/ui/tooltip"
-import { TanStackDevtools } from "@tanstack/react-devtools"
-import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools"
 import { createRootRouteWithContext, Outlet, redirect } from "@tanstack/react-router"
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools"
-import { useEffect, useState } from "react"
 import { CossistantChat } from "~/components/cossistant-chat"
 import { Toaster } from "~/components/toaster"
 import { siteConfig } from "~/config/site"
@@ -14,28 +10,6 @@ import type { trpcUtils } from "~/lib/trpc"
 
 export type RouterAppContext = {
   trpc: typeof trpcUtils
-}
-
-function Devtools() {
-  const [show, setShow] = useState(false)
-
-  useEffect(() => {
-    if (!document.querySelector("[data-testid='tanstack_devtools']")) {
-      setShow(true)
-    }
-  }, [])
-
-  if (!show) return null
-
-  return (
-    <TanStackDevtools
-      config={{ position: "bottom-left" }}
-      plugins={[
-        { name: "Tanstack Query", render: <ReactQueryDevtoolsPanel /> },
-        { name: "Tanstack Router", render: <TanStackRouterDevtoolsPanel /> },
-      ]}
-    />
-  )
 }
 
 function RootComponent() {
@@ -47,8 +21,6 @@ function RootComponent() {
         <Analytics clientId={env.VITE_OPENPANEL_CLIENT_ID} />
         <Toaster />
         <CossistantChat />
-
-        <Devtools />
       </TooltipProvider>
     </SupportProvider>
   )
